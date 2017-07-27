27a2d001db54af59ca04b90e05233d0d0f981c8d
trickpasp <patrickeeep12@gmail.com>
2017-07-27 10:37:17 -0300

    Alterei o nome do arquivo inicio para tipos_primitivos e adicionei o arquivo datas_e_horas

---
+++ js/datas_e_horas.js
@@ -0,0 +1,6 @@
+//Aprendendo o básico de manipulação de data e hora
+var nesse_tempo = new Date(2010, 0, 1); // 0 1º dia do 1º mês de 2010
+var ultimo = new Date(2010, 0, 1, 17, 10, 30); // o mesmo dia com hora local
+
+var agora = new Date(); // data e hora atuais
+var decorrido = agora - nesse_tempo; // subtração de data e horas atuais 
